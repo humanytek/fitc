8472e0cefee6fab1d9923e53757b36b1d8849563
Francisco Martinez <99762386+luisfra2385@users.noreply.github.com>
2026-09-19 23:22:19 -0600

    Update __manifest__.py
    Change version 17 -> 19

--- itc_internal_carta_porte/__manifest__.py
+++ itc_internal_carta_porte/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'ITC Internal Carta Porte',
-    'version': '17.0.1.0.0',
+    'version': '19.0.1.0.0',
     'summary': 'Habilita Carta Porte en traslados internos seleccionados',
     'category': 'Inventory/Inventory',
     'author': 'ITC',
     'license': 'LGPL-3',
     'depends': [
-        'l10n_mx_edi_stock_30',
+        'stock',
+        'l10n_mx_edi_stock',
     ],
     'data': [
         'views/stock_picking_type_views.xml',
